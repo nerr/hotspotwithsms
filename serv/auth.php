2de7fd9d5afe78ac1f5c916a79430801612ff1ab
@nerrsoft <leon@nerrsoft.com>
2013-02-28 16:29:28 +0800

    test php fetion class ok

--- serv/auth.php
+++ serv/auth.php
@@ -1,35 +1,46 @@
 <?php
-/*
- *
- */
 $config = array(
-	'pageTitle' = 'Free Wi-Fi Hotspot',
+	'fetionAccount'  => '13524289996',
+	'fetionPassword' => 'nerr5257',
+	'fetionTrytime'  => 3,
 );
 
-/*class hotspotsmsauth
+require 'class/PHPFetion.php';
+
+function check_status($status_code)
 {
-	
-	public function auth($mp, $pass)
+   preg_match( '/^.*HTTP\/1\.1 200 OK.*$/si', $status_code, $status );
+   if ( isset($status[0]) ){
+     return true;
+   }else{
+     echo "Result:$status_code\n";
+     return false;
+   }
+}
+
+/*$fetion = new PHPFetion($config['fetionAccount'], $config['fetionPassword']);
+try
+{
+	$result = $fetion->send($config['fetionAccount'], 'hello test');
+	$count = 0;
+	while( ! check_status( $result ) && $count < $config['fetionAccount'] )
 	{
-		$tm = now();
-		$sql = "select * from authlog where mobilephone=$mp and smspass=$pass and logtime<$tm";
+		$count++;
+		sleep(2);
+		echo "Sleep 2s, Re Send\n";
+		$result = $result = $fetion->send($config['fetionAccount'], 'hello test');
 	}
-
-	private function log2db()
-	{
-
+	if ( $count != ( $config['fetionAccount']-- ) ){
+		echo "Date:".date('Y-m-d H:i:s', time()).";Finished\n";
+	}else{
+		echo "Date:".date('Y-m-d H:i:s', time()).";Failed\n";
 	}
-	
-	private function sendsms()
-	{
-
-	}
-
-	private function makepass()
-	{
-
-		return $pass;
-	}
+}
+catch( Exception $e)
+{
+	echo "Date:".date('Y-m-d H:i:s', time()).";ERROR:".$e."\n";
 }*/
+
+
 
 ?>
