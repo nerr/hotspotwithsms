cdbf308d2fa3b67070c01593668f745c92b9046d
@nerrsoft <leon@nerrsoft.com>
2013-03-01 12:12:49 +0800

    finished

--- serv/auth.php
+++ serv/auth.php
@@ -1,47 +1,25 @@
 <?php
 $config = array(
-	'smsAccount'  => '13524289996',
-	'smsPassword' => 'nerr5257',
-	'smsTrytime'  => 3,
+	//-- SMS info
+	'smsAccount'    => '13524289996',
+	'smsPassword'   => 'nerr5257',
+	'smsTrytime'    => 3,
+	'smsTextPrefix' => '麻辣印象无线网络验证码:',
+	'smsTextSuffix' => ',验证码5分钟内有效.感谢您的光临,祝您用餐愉快.',
+
+	//-- MySQL info
+	'dbUser' => 'root',
+	'dbPass' => '911911',
+	'dbName' => 'hotspot',
+	'dbHost' => '127.0.0.1',
 );
 
-require 'class/PHPFetion.php';
+require 'class/hotspotsms.php';
 
+$hotspot = new hotspotsms($config);
 
-/*$fetion = new PHPFetion($config['fetionAccount'], $config['fetionPassword']);
-try
-{
-	$result = $fetion->send($config['fetionAccount'], 'hello test');
-	$count = 0;
-	while( ! check_status( $result ) && $count < $config['fetionAccount'] )
-	{
-		$count++;
-		sleep(2);
-		echo "Sleep 2s, Re Send\n";
-		$result = $result = $fetion->send($config['fetionAccount'], 'hello test');
-	}
-	if ( $count != ( $config['fetionAccount']-- ) ){
-		echo "Date:".date('Y-m-d H:i:s', time()).";Finished\n";
-	}else{
-		echo "Date:".date('Y-m-d H:i:s', time()).";Failed\n";
-	}
-}
-catch( Exception $e)
-{
-	echo "Date:".date('Y-m-d H:i:s', time()).";ERROR:".$e."\n";
-}*/
-
-
-
-function getRandCode($bit)
-{
-	$code = '';
-	for($i = 0; $i < $bit; $i++)
-	{
-		$code .= rand(0,9);
-	}
-	return $code;
-}
-
-
+if($_GET['action'] == 'step1')
+	$hotspot->step1();
+elseif($_GET['action'] == 'step2')
+	$hotspot->step2();
 ?>
--- serv/class/hotspotsms.php
+++ serv/class/hotspotsms.php
@@ -1,4 +1,5 @@
 <?php
+include 'PHPFetion.php';
 /**
  * Hotspot auth with SMS
  *
@@ -25,6 +26,19 @@ class hotspotsms
      */
     protected $_trytime;
 
+    /**
+     * 
+     * @var string
+     */
+    protected $_smsTextPrefix;
+    protected $_smsTextSuffix;
+
+    /**
+     * MySQL 连接
+     * @var object
+     */
+    protected $_mysqli;
+
 
 	/**
 	* 构造函数
@@ -35,25 +49,39 @@ class hotspotsms
 		$this->_mobile   = $cfg['smsAccount'];
 		$this->_password = $cfg['smsPassword'];
 		$this->_trytime  = $cfg['smsTrytime'];
+		$this->_smsTextPrefix = $cfg['smsTextPrefix'];
+		$this->_smsTextSuffix = $cfg['smsTextSuffix'];
+
+		$this->connect2db($cfg['dbHost'], $cfg['dbUser'], $cfg['dbPass'], $cfg['dbName']);
 	}
+	/**
+     * 析构函数
+     */
+    public function __destruct()
+    {
+        $this->_mysqli->close();
+    }
 
 	public function step1()
 	{
 		$logtime = time();
-		$mobile  = $_POST['mobile'];
+		$mobile  = $_GET['mobile'];
 
 		//-- 确认库中是否存在有效验证码 - 
-		//"select smspass from table where mobile=$mobile and logtime+600>$logtime order by logtime desc limit 1 ";
-		if()
+		$sql = "select smspass from log where mobile='$mobile' and logtime+300>$logtime order by logtime desc limit 1";
+		$res = mysqli_query($this->_mysqli, $sql);
+
+		if($res->num_rows > 0)
 		{
-			$smspass = $result['smspass'];
-			//-- insert log 2 db
+			$row = mysqli_fetch_assoc($res);
+			$smspass = $row['smspass'];
 		}
 		else
 		{
-			$smspass = $this->getRandCode();	
+			$smspass = $this->getRandCode();
+			$sql = "insert into log (mobile, smspass, logtime) values ('$mobile', '$smspass', $logtime)";
+			mysqli_query($this->_mysqli, $sql);
 		}
-		
 
 		//
 
@@ -66,6 +94,29 @@ class hotspotsms
 			$back['status'] = false;
 			$back['message'] = '短信发送失败,请确认您的手机号码后重新点击<获取>按钮';
 		}
+
+		echo $_GET['callback'].'('.json_encode($back).')';
+	}
+
+	public function step2()
+	{
+		$currtime = time();
+		$mobile  = $_POST['mobile'];
+		$smspass = $_POST['smspass'];
+
+		$sql = "select * from log where mobile='$mobile' and smspass='$smspass' and logtime+300>$currtime order by logtime desc limit 1";
+		$res = mysqli_query($this->_mysqli, $sql);
+		if($res->num_rows > 0)
+		{
+			$back['status'] = true;
+		}
+		else
+		{
+			$back['status'] = false;
+			$back['message'] = '验证失败,请确认您输入的验证码是否正确';
+		}
+
+		echo $_GET['callback'].'('.json_encode($back).')';
 	}
 
     /**
@@ -78,14 +129,14 @@ class hotspotsms
 		$fetion = new PHPFetion($this->_mobile, $this->_password);
 		try
 		{
-			$result = $fetion->send($mobile, $msg);
+			$result = $fetion->send($mobile, $this->_smsTextPrefix.$msg.$this->_smsTextSuffix);
 			$count = 0;
-			while(!check_status($result) && $count < $this->_trytime )
+			while(!$this->checkStatus($result) && $count < $this->_trytime )
 			{
 				$count++;
 				sleep(2);
 				//echo "Sleep 2s, Re Send\n";
-				$result = $result = $fetion->send($mobile, $msg);
+				$result = $result = $fetion->send($mobile, $this->_smsTextPrefix.$msg.$this->_smsTextSuffix);
 			}
 			if ($count != ($this->_trytime - 1))
 			{
@@ -128,10 +179,19 @@ class hotspotsms
 	protected function getRandCode($digits = 6)
 	{
 		$code = '';
-		for($i = 0; $i < $bit; $i++)
+		for($i = 0; $i < $digits; $i++)
 		{
 			$code .= rand(0,9);
 		}
 		return $code;
+	}
+
+	/**
+	 * 链接MySQL
+	 * @param string $digits = 6; 随机码位数
+	 */
+	protected function connect2db($host, $user, $pass, $db)
+	{
+		$this->_mysqli = new mysqli($host, $user, $pass, $db);
 	}
 }
