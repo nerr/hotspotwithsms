2bb92fffa29e50020ae5cf855f3e44fe8527f72f
@nerrsoft <leon@nerrsoft.com>
2013-03-01 14:32:02 +0800

    mv  the config info to config.php

--- serv/auth.php
+++ serv/auth.php
@@ -1,19 +1,5 @@
 <?php
-$config = array(
-	//-- SMS info
-	'smsAccount'    => '13524289996',
-	'smsPassword'   => 'nerr5257',
-	'smsTrytime'    => 3,
-	'smsTextPrefix' => '麻辣印象无线网络验证码:',
-	'smsTextSuffix' => ',验证码5分钟内有效.感谢您的光临,祝您用餐愉快.',
-
-	//-- MySQL info
-	'dbUser' => 'root',
-	'dbPass' => 'nerrsoft',
-	'dbName' => 'hotspot',
-	'dbHost' => '127.0.0.1',
-);
-
+include 'config.php';
 require 'class/hotspotsms.php';
 
 $hotspot = new hotspotsms($config);
